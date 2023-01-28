ae9b7f365f762e515d9619e14fb22b975ff18381
TahaAnsari654 <123648959+TahaAnsari654@users.noreply.github.com>
2023-01-28 20:53:29 +0500

    javascript first and second assignment

---
+++ class 2/app.js
@@ -0,0 +1,98 @@
+// chapter 2
+
+//Question 1
+// var username
+
+
+
+//Question 2
+
+// var myname="Syed Bilal Hasan"
+
+// console.log(myname)
+
+
+//Question 3
+
+
+// var massage
+//  massage ="Hello world"
+
+// alert(massage)
+
+
+//Question 4
+
+// var data = "my name is bilal \n my father name is Azeem uddin \n and my age is 20 years"
+
+// alert(data)
+
+//Question 5
+
+// var display = "pizza \npizz \npiz \npi \np"
+// alert(display)
+
+//Question 6
+
+// var mail = "my Email address is "
+// var email = "example@example.com"
+// alert(mail + email)
+
+
+//Question 7
+
+// var book = "i am trying to learn from the book A smarter way to learn javascript"
+// alert(book)
+
+
+//Question 8
+
+// document.write("yah! i can write HTML content through javascript")
+
+//Question 9
+
+// var line ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(line)
+
+
+
+
+
+//chapter 3
+
+//Question 1
+
+// var age ="i am 15 years old"
+// alert(age)
+
+//Question 3
+
+// var birthYear = 2002
+// document.write("MY birthday is ",birthYear)
+
+
+//Question 4
+
+// var Visitor’sName = "Bilal"
+// var ProductTitle = "Clothes" 
+// var quantity = 5
+// document.write(Visitor’sName,ProductTitle,uantity)
+
+
+//chapter 4
+//Question 1
+
+// var a,b,c;
+
+//Question 2
+//illegal
+// var tou&I, first name, 3rd-Number, %myname, #myname
+
+//llegal
+// var fav movie,aLongVariableName,X,_my_name,$my_name
+
+
+//Question 3
+
+
+document.write("a) A heading stating “Rules for naming JS variables \n b) Variable names can only contain ______, ______,______ and ______.For example $my_1stVariable \n c) Variables must begin with a ______, ______ or____.For example $name, _name or name \n d) Variable names are case _________e) Variable names should not be JS _________)")
---
+++ class1/app.js
@@ -0,0 +1,20 @@
+// chaper 1
+
+//Question 1
+
+// alert("hello world")
+
+//Question 2
+
+// var fullname = "syed bilal \n hasan"
+
+// alert(fullname)
+
+
+//Question 3
+
+// var javascript = "welcome to js land...."
+
+// alert(javascript)
+
+
