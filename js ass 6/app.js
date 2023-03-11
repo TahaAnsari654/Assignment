e5173516c3715789d633bcd33f74867d7434ca17
TahaAnsari654 <123648959+TahaAnsari654@users.noreply.github.com>
2023-03-11 06:50:21 -0800

    javascript assigment 6

---
+++ js ass 6/app.js
@@ -0,0 +1,56 @@
+//assigmrnt 6
+// chapter 21 to 25
+// question 1
+
+// var name1 = prompt("enter your first name");
+// var name2 = prompt("enter your last name");
+// var tittle = prompt("enter your full name ")
+
+
+//question 2
+
+// var str = "samsung galaxy s6 edge plus";
+// document.write(str.length)
+
+//question 3
+
+//  var str = "Pakistani";
+//  document.write ( str.indexOf ("n"))
+
+//question 4
+// var str = "Hello world";
+// document.write( str.lastIndexOf("l"))
+
+//question 5
+
+// var str = "Pakistani";
+// document.write(str.charAt("3"))
+
+//question 6
+
+//  var str = " hyderabad"
+//  document.write(str.replace("hyderabad" , "Islamabad")) 
+
+//question7
+
+// var massage = " Ali and sami are best friend . The play cricket and foot ball together "
+// document.write(massage.replaceAll("and" , "&"))
+
+//question 8
+
+// var num = "10";
+// document.write(num++)
+
+
+//question 9
+
+// var food = "peanuts"
+// document.write(food.toUpperCase(food))
+
+//question 10
+
+// var userinput = "javascript";
+// var result = userinput[0].toUpperCase()+userinput.slice(1)
+// document.write (result)
+
+
